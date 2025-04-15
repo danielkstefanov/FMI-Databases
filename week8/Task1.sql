bef5bba529b6597a0b29707d015bc4d714f30d89
Daniel Stefanov <97540472+danielkstefanov@users.noreply.github.com>
2025-04-15 15:21:15 +0300

    Add solutions to tasks from week 8

---
+++ week8/Task1.sql
@@ -0,0 +1,60 @@
+﻿
+CREATE DATABASE TECHSHOP;
+
+USE TECHSHOP
+
+--Задача 1
+
+--а) Дефинирайте следните релации:
+
+---Product (maker, model, type), където:
+--- модел е низ от точно 4 символа,
+--- производител е низ от точно 1 символ,
+--- тип е низ до 7 символа;
+
+CREATE TABLE Product (
+	model CHAR(4),
+	producer CHAR(1),
+	type VARCHAR(7),
+)
+
+---Printer (code, model, price), където:
+--- код е цяло число,
+--- модел е низ от точно 4 символа,
+--- цена с точност до два знака след десетичната запетая;
+
+CREATE TABLE Printer (
+	code INTEGER,
+	model CHAR(4),
+	price DECIMAL(8,2)
+)
+
+--б) Добавете кортежи с примерни данни към новосъздадените релации.
+
+INSERT INTO Product VALUES 
+	('KJOF', 'R', 'fast'),
+	('PGRG', 'P', 'slow'),
+	('LOKM', 'F', 'cool')
+
+INSERT INTO Printer VALUES 
+	(244, 'KJOF', 123.45),
+	(544, 'PGRG', 543.23),
+	(122, 'LOKM', 99.99)
+
+
+--в) Добавете към релацията Printer атрибути:
+
+--- type - низ до 6 символа (забележка: type може да приема
+--- стойност 'laser', 'matrix' или 'jet'),
+--- color - низ от точно 1 символ, стойност по подразбиране 'n'
+--(забележка: color може да приема стойност 'y' или 'n').
+
+ALTER TABLE Printer ADD type VARCHAR(6), color CHAR(1);
+
+--г) Напишете заявка, която премахва атрибута price от релацията Printer.
+
+ALTER TABLE Printer DROP COLUMN price;
+
+--д) Изтрийте релациите, които сте създали в Задача 1.
+
+DROP DATABASE TECHSHOP
---
+++ week8/Task2.sql
@@ -0,0 +1,64 @@
+﻿--Задача 2
+
+CREATE DATABASE Facebook
+USE Facebook
+
+--а) Нека създадем мини вариант на Facebook. Искаме да имаме следните
+--релации (може да предложите и друг вариант):
+
+--- Users: уникален номер (id), email, парола, дата на регистрация.
+
+CREATE TABLE Users (
+	id INT IDENTITY(1,1),
+	email VARCHAR(64),
+	password VARCHAR(64),
+	registrationDate DATE
+)
+
+--- Friends: двойки от номера на потребители, напр. ако 12 е приятел на 21, 25 и
+--40, ще има кортежи (12,21), (12,25), (12,40).
+
+CREATE TABLE Friends (
+	firstUser INT NOT NULL,
+	secondUser INT NOT NULL
+)
+
+--- Walls: номер на потребител, номер на потребител написал съобщението,
+--текст на съобщението, дата.
+
+CREATE TABLE Walls (
+	userId INT NOT NULL,
+	text VARCHAR(256),
+	date DATETIME
+)
+
+--- Groups: уникален номер, име, описание (по подразбиране - празен низ).
+
+CREATE TABLE Groups (
+	id INT IDENTITY(1,1),
+	name VARCHAR(64),
+	description VARCHAR(512) DEFAULT ''
+)
+
+--- GroupMembers: двойки от вида номер на група - номер на потребител.
+
+CREATE TABLE GroupMembers (
+	userId INT NOT NULL,
+	groupId INT NOT NULL
+)
+
+--б) Добавете кортежи с примерни данни към новосъздадените релации.
+
+INSERT INTO Users VALUES
+('vasiprasi@abv.bg','password123', '2025-04-14'),
+('danibanani@abv.bg','daniPass123', '2025-03-14')
+
+INSERT INTO Friends VALUES 
+(1,2)
+
+INSERT INTO Groups VALUES 
+('Corgi lovers', 'Group for all the lovers of the corgi breed!')
+
+INSERT INTO GroupMembers VALUES
+(1,1),
+(2,1)
